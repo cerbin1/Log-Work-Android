6dadd54152fcc6145364acf8a9d6bd50b21fce78
Bartek <bartek9393@poczta.onet.pl>
2017-08-19 23:55:07 +0200

    Create methods for saving working hours in a file

--- app/src/main/java/com/example/bartek/log_work_android/MainActivity.java
+++ app/src/main/java/com/example/bartek/log_work_android/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.io.InputStreamReader;
 import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +27,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setButtonsListeners() {
-        Button addWorkHoursButton = (Button) findViewById(R.id.displayHoursButton);
-        addWorkHoursButton.setOnClickListener(displayWorkingHoursCount());
+        Button addWorkingHoursButton = (Button) findViewById(R.id.addHoursButton);
+        addWorkingHoursButton.setOnClickListener(addWorkingHours());
+
+        Button displayWorkingHoursButton = (Button) findViewById(R.id.displayHoursButtonId);
+        displayWorkingHoursButton.setOnClickListener(displayWorkingHours());
     }
 
     @NonNull
-    private View.OnClickListener displayWorkingHoursCount() {
+    private View.OnClickListener addWorkingHours() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Context context = MainActivity.this;
-                try {
-                    displayWorkingHoursCount(context);
-                } catch (IOException e) {
-                    Log.d("IOException", e.getMessage());
-                }
+                int workingHours = Integer.parseInt(getWorkingHoursCount());
+                EditText workingHoursEditText = (EditText) findViewById(R.id.workingHoursEditText);
+                workingHours += Integer.parseInt(workingHoursEditText.getText().toString());
+                saveWorkingHours();
             }
         };
     }
 
-    private void displayWorkingHoursCount(Context context) throws IOException {
-        InputStream inputStream = context.getAssets().open("working_hours.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    private void saveWorkingHours() {
+        
+    }
 
-        String message = "Hours worked: " + reader.readLine();
-        makeText(context, message, LENGTH_LONG).show();
+    @NonNull
+    private View.OnClickListener displayWorkingHours() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "Hours worked: " + getWorkingHoursCount();
+                makeText(context, message, LENGTH_LONG).show();
+            }
+        };
+    }
 
-        reader.close();
-        inputStream.close();
-        context.getAssets().close();
+    private String getWorkingHoursCount() {
+        try {
+            InputStream inputStream = context.getAssets().open("working_hours.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String message = "Hours worked: " + reader.readLine();
+            makeText(context, message, LENGTH_LONG).show();
+
+            reader.close();
+            inputStream.close();
+            context.getAssets().close();
+            return message;
+        } catch (IOException e) {
+            Log.d("IOException", e.getMessage());
+        }
+        return "";
     }
 }
