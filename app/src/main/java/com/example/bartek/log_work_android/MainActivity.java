425597eb1efb5aa29a94090863cd91d5fe256ec4
Bartek <bartek9393@poczta.onet.pl>
2017-08-20 15:00:55 +0200

    Changed saving data to file in internal storage, implemented saving working hours

--- app/src/main/java/com/example/bartek/log_work_android/MainActivity.java
+++ app/src/main/java/com/example/bartek/log_work_android/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.bartek.log_work_android;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import static android.widget.Toast.*;
+import static android.widget.Toast.LENGTH_LONG;
+import static android.widget.Toast.makeText;
 
 public class MainActivity extends AppCompatActivity {
     private Context context = MainActivity.this;
@@ -39,16 +42,27 @@ public class MainActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int workingHours = Integer.parseInt(getWorkingHoursCount());
-                EditText workingHoursEditText = (EditText) findViewById(R.id.workingHoursEditText);
-                workingHours += Integer.parseInt(workingHoursEditText.getText().toString());
                 saveWorkingHours();
             }
         };
     }
 
     private void saveWorkingHours() {
-        
+        int workingHours = Integer.parseInt(getWorkingHoursCount());
+        EditText workingHoursEditText = (EditText) findViewById(R.id.workingHoursEditText);
+        workingHours += Integer.parseInt(workingHoursEditText.getText().toString());
+        String FILENAME = "test.txt";
+        String string = Integer.toString(workingHours);
+
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(string.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d("FileNotFoundException", e.getMessage());
+        } catch (IOException e) {
+            Log.d("IOException", e.getMessage());
+        }
     }
 
     @NonNull
@@ -64,16 +78,15 @@ public class MainActivity extends AppCompatActivity {
 
     private String getWorkingHoursCount() {
         try {
-            InputStream inputStream = context.getAssets().open("working_hours.txt");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            FileInputStream fis = context.openFileInput("test.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            String workingHours = bufferedReader.readLine();
 
-            String message = "Hours worked: " + reader.readLine();
-            makeText(context, message, LENGTH_LONG).show();
-
-            reader.close();
-            inputStream.close();
-            context.getAssets().close();
-            return message;
+            fis.close();
+            isr.close();
+            bufferedReader.close();
+            return workingHours;
         } catch (IOException e) {
             Log.d("IOException", e.getMessage());
         }
