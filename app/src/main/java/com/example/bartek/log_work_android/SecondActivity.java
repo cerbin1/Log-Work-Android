12a076638b65c4b1102bad76fae26bd8a651a404
Bartek <bartek9393@poczta.onet.pl>
2017-08-22 21:12:49 +0200

    Added dialog to delete sum of worked hours

--- app/src/main/java/com/example/bartek/log_work_android/SecondActivity.java
+++ app/src/main/java/com/example/bartek/log_work_android/SecondActivity.java
@@ -1,8 +1,14 @@
 package com.example.bartek.log_work_android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,5 +18,23 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
         TextView workedHoursTextView = (TextView) findViewById(R.id.sumOfWorkedHours);
         workedHoursTextView.setText(getIntent().getExtras().getString("string"));
+    }
+
+    public void deleteSumOfWorkedHours(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to delete sum of worked hours?")
+                .setCancelable(false)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        makeText(SecondActivity.this, "Deleted", LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
