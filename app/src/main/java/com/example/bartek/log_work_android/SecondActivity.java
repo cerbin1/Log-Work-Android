436f1088ca164b052129092fba9cddd97ce0f5e5
Bartek <bartek9393@poczta.onet.pl>
2017-08-23 18:32:12 +0200

    Implemented clearing sum of worked hours on button click

--- app/src/main/java/com/example/bartek/log_work_android/SecondActivity.java
+++ app/src/main/java/com/example/bartek/log_work_android/SecondActivity.java
@@ -1,23 +1,30 @@
 package com.example.bartek.log_work_android;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        TextView workedHoursTextView = (TextView) findViewById(R.id.sumOfWorkedHours);
-        workedHoursTextView.setText(getIntent().getExtras().getString("string"));
+        textView = (TextView) findViewById(R.id.sumOfWorkedHours);
+        textView.setText(getIntent().getExtras().getString("string"));
     }
 
     public void deleteSumOfWorkedHours(View view) {
@@ -27,6 +34,16 @@ public class SecondActivity extends AppCompatActivity {
                 .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         makeText(SecondActivity.this, "Deleted", LENGTH_SHORT).show();
+                        try {
+                            FileOutputStream outputStream = openFileOutput("sum_of_worked_hours.txt", Context.MODE_PRIVATE);
+                            outputStream.write("0".getBytes());
+                            outputStream.close();
+                            textView.setText("0");
+                        } catch (FileNotFoundException e) {
+                            Log.e("", "FileNotFoundException " + e.getMessage());
+                        } catch (IOException e) {
+                            Log.e("", "IOException " + e.getMessage());
+                        }
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
