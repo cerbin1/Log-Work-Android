ba834f4d979695018070726c40e0ef296e9f46ea
Bartek <bartek9393@poczta.onet.pl>
2017-09-10 15:29:58 +0200

    Added class DatabaseHelper

---
+++ app/src/main/java/com/example/bartek/log_work_android/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.example.bartek.log_work_android;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "work.db";
+    public static final int DATABASE_VERSION = 1;
+
+    private final static String ID = "Id";
+    private final static String DAY_NUMBER = "Day";
+    private final static String MONTH_NUMBER = "Month";
+    private final static String YEAR = "Year";
+    private final static String DAY_NAME = "Day name";
+    private final static String HOURS_WORKED = "Hours worked";
+
+    private static final String TABLE_NAME = "LOG_WORK";
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + TABLE_NAME + "("
+            + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + DAY_NUMBER + " INTEGER, "
+            + MONTH_NUMBER + " INTEGER, "
+            + YEAR + " INTEGER"
+            + DAY_NAME + " TEXT, "
+            + HOURS_WORKED + " FLOAT)";
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
+    }
+}
